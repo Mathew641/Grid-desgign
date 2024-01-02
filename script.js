d7bc05bbc4724652255f618a0614ae7b726b9c2f
Mathew641 <106030476+Mathew641@users.noreply.github.com>
2024-01-02 07:40:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// alert("Welcome to the Tutorial -44 of Sigma Web Development for Designing the Grid");
+
+console.log("Welcome to the Tutorial-44 for Designing of Grid in Sigma Web Development Course!!!");
+
